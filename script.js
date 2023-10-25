54d8acc23f8f9c8ec7a40f16aa3135375f890b40
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-10-25 15:33:36 +0200

    game now has UI, only missing counters and win announcement

--- script.js
+++ script.js
@@ -1,58 +1,60 @@
 const rps = ["Rock", "Paper", "Scissors"];
 const computerChoice = rps[Math.floor((Math.random() * rps.length))];
 console.log("Computer choose: " + computerChoice);
-      
-//player part
-// const userChoice = prompt("Choose your weapon: Rock, Paper or Scissors").toLowerCase();
-// console.log("You choose: " + userChoice);
 
 let userChoice ="";
-
+let winningString = "";
 
 document.getElementById("btnRock").addEventListener("click",() => {
     userChoice = "Rock";
     console.log("You choose Rock!")
     gameRound(computerChoice,userChoice);
+    updateUserChoice();
+    updateWinningString();
 })
 
 document.getElementById("btnPaper").addEventListener("click",() => {
     userChoice = "Paper";
-    console.log("You choose Paper!")
+    console.log("You choose Paper!");
     gameRound(computerChoice,userChoice);
+    updateUserChoice();
+    updateWinningString();
 })
 
 document.getElementById("btnScissors").addEventListener("click",() => {
     userChoice = "Scissors";
-    console.log("You choose Scissors!")
+    console.log("You choose Scissors!");  
     gameRound(computerChoice,userChoice);
+    updateUserChoice();
+    updateWinningString();
 })
 
 
+let paraComputerChoose = document.querySelector("#computerChoose");
+paraComputerChoose.textContent = "Computer choose: " + computerChoice;
 
+function updateUserChoice(){
+    let paraUserChoose = document.querySelector("#userChoose");
+    paraUserChoose.textContent = "You choose: " + userChoice;
+}
 
-
-
-
-
-
-
-
-
-
-
+function updateWinningString(){
+let paraResult = document.querySelector("#roundResult");
+paraResult.textContent = winningString;
+}
 
 
 function gameRound(computerChoice,userChoice){
     if (computerChoice === userChoice){
-        console.log("Its a draw!");
-    }else if (computerChoice == "rock" && userChoice == "scissors"){
-        console.log("Computer won!");
-    } else if(computerChoice == "paper" && userChoice == "rock"){
-        console.log("Computer won!");
-    } else if ( computerChoice == "scissors" && userChoice == "paper"){
-        console.log("Computer won!");
+        return winningString = "Its a draw";
+    }else if (computerChoice == "Rock" && userChoice == "Scissors"){
+        return winningString = "Computer won!";
+    } else if(computerChoice == "Paper" && userChoice == "Rock"){
+        return winningString = "Computer won!";
+    } else if ( computerChoice == "Scissors" && userChoice == "Paper"){
+        return winningString = "Computer won!";
     } else {
-        console.log("Player won!");
+        return winningString = "Player won!";
     }
 }
 
