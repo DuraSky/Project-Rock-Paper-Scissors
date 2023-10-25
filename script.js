a83e1c95a5abf34478ef224f1a74f190c56689be
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-10-25 23:13:12 +0200

    finish with the code, ui now fully works with running score and alerts once either player or computer gets to 5, had to rework a lot of code

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-const rps = ["Rock", "Paper", "Scissors"];
-const computerChoice = rps[Math.floor((Math.random() * rps.length))];
-console.log("Computer choose: " + computerChoice);
-
 let userChoice ="";
 let winningString = "";
+let playerCounter = 0;
+let computerCounter = 0;
+
 
 document.getElementById("btnRock").addEventListener("click",() => {
     userChoice = "Rock";
-    console.log("You choose Rock!")
-    gameRound(computerChoice,userChoice);
+    gameChoice();
     updateUserChoice();
     updateWinningString();
+    updateScore();
 })
 
 document.getElementById("btnPaper").addEventListener("click",() => {
     userChoice = "Paper";
-    console.log("You choose Paper!");
-    gameRound(computerChoice,userChoice);
+    gameChoice();
     updateUserChoice();
     updateWinningString();
+    updateScore();
 })
 
 document.getElementById("btnScissors").addEventListener("click",() => {
     userChoice = "Scissors";
-    console.log("You choose Scissors!");  
-    gameRound(computerChoice,userChoice);
+    gameChoice();
     updateUserChoice();
     updateWinningString();
+    updateScore();
 })
 
+function updateScore(){
+    document.getElementById("computerScore").innerHTML = computerCounter;
+    document.getElementById("playerScore").innerHTML = playerCounter;
+}
 
-let paraComputerChoose = document.querySelector("#computerChoose");
-paraComputerChoose.textContent = "Computer choose: " + computerChoice;
+function gameChoice(){
+    const rps = ["Rock", "Paper", "Scissors"];
+    const computerChoice = rps[Math.floor((Math.random() * rps.length))];
+    console.log("Computer choose: " + computerChoice);
+
+    let paraComputerChoose = document.querySelector("#computerChoose");
+    paraComputerChoose.textContent = "Computer choose: " + computerChoice;
+
+    gameRound(computerChoice, userChoice);
+    checkTheScore();
+}
 
 function updateUserChoice(){
     let paraUserChoose = document.querySelector("#userChoose");
@@ -39,8 +51,8 @@ function updateUserChoice(){
 }
 
 function updateWinningString(){
-let paraResult = document.querySelector("#roundResult");
-paraResult.textContent = winningString;
+    let paraResult = document.querySelector("#roundResult");
+    paraResult.textContent = winningString;
 }
 
 
@@ -48,14 +60,29 @@ function gameRound(computerChoice,userChoice){
     if (computerChoice === userChoice){
         return winningString = "Its a draw";
     }else if (computerChoice == "Rock" && userChoice == "Scissors"){
+        computerCounter++;
         return winningString = "Computer won!";
     } else if(computerChoice == "Paper" && userChoice == "Rock"){
+        computerCounter++;
         return winningString = "Computer won!";
     } else if ( computerChoice == "Scissors" && userChoice == "Paper"){
+        computerCounter++;
         return winningString = "Computer won!";
     } else {
+        playerCounter++;
         return winningString = "Player won!";
+        
     }
 }
+
+function checkTheScore(){
+    if (playerCounter === 5) {
+    alert("Player has won the game!");
+    }else if (computerCounter === 5){
+    alert("Computer has won the game!");
+    }
+}
+
+
 
    
